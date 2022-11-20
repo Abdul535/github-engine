9a113208bd314b069562c6f602130ea4960c6b6d
Abdul535 <syedabdullah535@gmail.com>
2022-11-20 18:09:17 +0530

    done with css, search left

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,6 @@ function NavBar() {
     <div className="navbar">
       <Link to="/"> <i  className="fa-brands fa-github fa-2x"></i><p className="navbar-se"> Github Search Engine</p></Link>
       <div>
-        <Link to="/contact">Contact us</Link>
-        <Link to="/about">About us</Link>
       </div>
     </div>
   )
